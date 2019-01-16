1c1e02636874bdc2a363a7573c67aac800dde7da
Sasha.Mykytyuk86 <Sasha.Mykytyuk86@gmail.com>
2019-01-16 17:48:42 +0200

    version 3

--- src/conponents/App.js
+++ src/conponents/App.js
@@ -5,8 +5,8 @@ class App extends React.Component {
     state = {
         ccy: [],
         summ: {},
-        isLoad: false,
-        buyOrSale: {}
+        isLoad: false
+        //buyOrSale: {}
     }
 
     componentDidMount() {
@@ -37,54 +37,93 @@ class App extends React.Component {
     onHandlerChangeSelect = (seletData) => {
         const val = seletData.target.value;
         const find = this.state.ccy.find((item) => item.ccy === val);
-        if(find)
-        {
-            if(this.state.buyOrSale === 'buy'){
-                console.log(this.state.summ * find.buy);
-            }
-            else{
-                console.log(this.state.summ * find.sale);
-            }
+        if (find) {
+            // if(this.state.buyOrSale === 'buy'){
+            //     console.log(this.state.summ * find.buy);
+            // }
+            // else{
+            //     console.log(this.state.summ * find.sale);
+            // }
         }
     }
 
 
-    onSelectBuyOrSale = (selectBuyOrSale) =>{
-        this.setState({ buyOrSale: selectBuyOrSale.target.value })
-    } 
+    // onSelectBuyOrSale = (selectBuyOrSale) =>{
+    //     this.setState({ buyOrSale: selectBuyOrSale.target.value })
+    // } 
 
 
 
     render() {
         //const isLoad = this.state.isLoad;
         const { isLoad } = this.state;
-        const options = this.state.ccy.map((c)=>{
-            return(
+        const options = this.state.ccy.map((c) => {
+            return (
                 <option key={c.ccy}>{c.ccy}</option>
             );
         });
         return (
             <div className='container'>
-                
-                <div>
-                    <label className='label'>input sum</label>
-                    <input type='text' onChange={this.onHandlerChange}></input>
-                    {isLoad && <span>Loading</span>}
-                    {!isLoad &&
-                        <select onChange = {this.onHandlerChangeSelect}>
-                            <option></option>
-                            {options}
-                        </select>
-                    }
-                    <select onChange = {this.onSelectBuyOrSale}>
-                        <option>buy</option>
-                        <option>sale</option>
-                    </select>
+                <div className='text-center'>
+                    <h2>КОНВЕРТЕР ВАЛЮТ</h2>
+                </div>
+
+                <div class="row" >
+                    <div class="col-md-2 mb-2">
+                        <label for="i_change">Міняю</label>
+                        <input type="text" id='i_change' class='form-control' onChange={this.onHandlerChange}/>
+                        {isLoad && <span>Loading</span>}
+                            {!isLoad &&
+                                <select onChange={this.onHandlerChangeSelect}>
+                                    <option></option>
+                                    {options}
+                                </select>
+                            }
+                        {/* <div class="invalid-feedback">
+                            Valid first name is required.
+                            </div> */}
+                    </div>
+                    <div>
+                        <img src="https://finance.liga.net/design/images/icons/converter-min-arr.png" />
+                    </div>
+
+                    <div class="col-md-2 mb-2">
+                        <label for="i_get">Отримую</label>
+                        <input type="text" class="form-control" readOnly='true' id="i_get" />
+                        {isLoad && <span>Loading</span>}
+                            {!isLoad &&
+                                <select onChange={this.onHandlerChangeSelect}>
+                                    <option></option>
+                                    {options}
+                                </select>
+                            }
+                        {/* <div class="invalid-feedback">
+                            Valid last name is required.
+                            </div> */}
+                    </div>
                 </div>
             </div>
         );
+        }
+
     }
-}
+
+
+
+
+                    {/* <div>
+                                <label className='label'>input sum</label>
+                            <input type='text' onChange={this.onHandlerChange}></input>
+                            {isLoad && <span>Loading</span>}
+                            {!isLoad &&
+                                <select onChange={this.onHandlerChangeSelect}>
+                                    <option></option>
+                                    {options}
+                                </select>
+                            }
+                            */}
+
+
 
 
 export default App;
