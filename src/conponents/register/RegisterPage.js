348bb6abe6aa09905c6a1efc701e8cf0d8b33d13
Sasha.Mykytyuk86 <Sasha.Mykytyuk86@gmail.com>
2019-01-18 21:32:16 +0200

    Added registration page

--- src/conponents/register/RegisterPage.js
+++ src/conponents/register/RegisterPage.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
-class SignupPage extends React.Component {
+class RegisterPage extends React.Component {
+
+    state = {
+        userName:{},
+        email:{},
+        password:{},
+        passwordConfirm:{}
+    }
+    onRegister = () =>{
+
+    }
+
+
     render() {
         return (
             <div class="container">
@@ -8,16 +20,24 @@ class SignupPage extends React.Component {
                     <div class="col-md-4 col-md-offset-4">
                         <div class="panel panel-default">
                             <div class="panel-heading">
-                                <h3 class="panel-title">Login</h3>
+                                <h3 class="panel-title">Registration</h3>
                             </div>
                             <div class="panel-body">
-                               <div class="form-group">
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="your name" name="name" type="text" />
+                                </div>
+                                <div class="form-group">
                                     <input class="form-control" placeholder="yourmail@example.com" name="email" type="text" />
                                 </div>
                                 <div class="form-group">
                                     <input class="form-control" placeholder="Password" name="password" type="password" value="" />
                                 </div>
-                                <input class="btn btn-lg btn-success btn-block" type="button" value="Login"/>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="PasswordConfirm" name="passwordConfirm" type="password" value="" />
+                                </div>
+                                <input class="btn btn-lg btn-success btn-block" type="button" onClick={this.onRegister} value="Register" />
+
+
                             </div>
                         </div>
                     </div>
@@ -25,7 +45,6 @@ class SignupPage extends React.Component {
             </div>
         );
     }
-
 }
 
-export default SignupPage;
+export default RegisterPage;
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import{Route, IndexRoute} from 'react-router';
 import App from './conponents/App';
 import Home from './conponents/Home';
 import SignupPage from './conponents/signup/SignupPage';
+import RegisterPage from './conponents/register/RegisterPage';
 import About from './conponents/About';
 
 //dev branch
@@ -12,6 +13,7 @@ export default(
     <Route path="/" component={App}>
     <IndexRoute component={Home} />
     <Route path="/signup" component={SignupPage}></Route>
+    <Route path='/register' component={RegisterPage}></Route>
     <Route path="/about" component={About}></Route>
 
     </Route>
